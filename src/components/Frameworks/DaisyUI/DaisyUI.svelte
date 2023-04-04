f1ef06eb3ba77d1e6242e5fc4aeff8160cbff176
Simon Dold <48808400+doldsimo@users.noreply.github.com>
2023-04-04 17:58:12 +0200

    adding components daisyui

--- src/components/Frameworks/DaisyUI/DaisyUI.svelte
+++ src/components/Frameworks/DaisyUI/DaisyUI.svelte
@@ -22,11 +22,130 @@
             >Click me</button
         >
     </div>
-</div>
+    <div class="sub-container-button">
+        <input type="checkbox" class="toggle toggle-primary" checked />
+    </div>
+    <div class="sub-container-button">
+        <div class="form-control">
+            <label class="label cursor-pointer">
+                <span class="label-text">On</span>
+                <input
+                    type="radio"
+                    name="radio-10"
+                    class="radio checked:bg-red-500"
+                    checked
+                />
+            </label>
+        </div>
+        <div class="form-control">
+            <label class="label cursor-pointer">
+                <span class="label-text">Off</span>
+                <input
+                    type="radio"
+                    name="radio-10"
+                    class="radio checked:bg-blue-500"
+                    checked
+                />
+            </label>
+        </div>
+    </div>
+    <div class="sub-container-button">
+        <label class="label cursor-pointer">
+            <input type="checkbox" checked="checked" class="checkbox" />
+            <span class="label-text ml-3">Check me</span>
+        </label>
+    </div>
+    <div class="sub-container-button" />
+    <div class="sub-container-button">
+        <div class="btn-group">
+            <input type="radio" name="options" data-title="One" class="btn" />
+            <input
+                type="radio"
+                name="options"
+                data-title="Two"
+                class="btn"
+                checked
+            />
+            <input type="radio" name="options" data-title="Three" class="btn" />
+        </div>
+    </div>
+    <div class="sub-container-button">
+        <!-- The button to open modal -->
+        <label for="my-modal" class="btn">open modal</label>
 
-<!-- styles must be added to component which is using tailwind classes -->
-<!-- <style global lang="postcss">
-    @tailwind base;
-    @tailwind components;
-    @tailwind utilities;
-</style>  -->
+        <!-- Put this part before </body> tag -->
+        <input type="checkbox" id="my-modal" class="modal-toggle" />
+        <div class="modal">
+            <div class="modal-box">
+                <h3 class="font-bold text-lg">
+                    Congratulations random Internet user!
+                </h3>
+                <p class="py-4">
+                    You've been selected for a chance to get one year of
+                    subscription to use Wikipedia for free!
+                </p>
+                <div class="modal-action">
+                    <label for="my-modal" class="btn">Yay!</label>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="sub-container-button">
+        <input
+            type="text"
+            placeholder="Your email"
+            class="input w-full max-w-xs"
+        />
+    </div>
+    <div
+        class="sub-container-button"
+        style="width: 100%; height: 100%; margin-top: 1em; margin-bottom: 1em"
+    >
+        <ul class="steps steps-vertical">
+            <li class="step step-primary">Bullet One</li>
+            <li class="step step-primary">Bullet Two</li>
+            <li class="step">Bullet Three</li>
+            <li class="step">Bullet Four</li>
+        </ul>
+    </div>
+    <div class="sub-container-button">
+        <input
+            type="range"
+            min="0"
+            max="100"
+            value="40"
+            class="range range-primary"
+        />
+    </div>
+    <div class="sub-container-button" style="height: 100%; margin-top: 1em; margin-bottom: 1em">
+        <table class="table w-full">
+            <!-- head -->
+            <thead>
+                <tr>
+                    <th />
+                    <th>Elements</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- row 1 -->
+                <tr>
+                    <th>0</th>
+                    <td>List Element 0</td>
+   
+                </tr>
+                <!-- row 2 -->
+                <tr>
+                    <th>1</th>
+                    <td>List Element 1</td>
+
+                </tr>
+                <!-- row 3 -->
+                <tr>
+                    <th>2</th>
+                    <td>List Element 2</td>
+
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
--- src/components/Frameworks/HeadlessUI/HeadlessUI.svelte
+++ src/components/Frameworks/HeadlessUI/HeadlessUI.svelte
@@ -1,6 +1,7 @@
 <script>
     // @ts-nocheck
     import Dialog from "./Dialog/Dialog.svelte";
+    import { Switch } from "@rgossiaux/svelte-headlessui";
     import { primaryColor, secondaryColor } from "../../../store/stores";
 
     let colorP;
@@ -13,9 +14,11 @@
     secondaryColor.subscribe((color) => {
         colorS = color;
     });
+
+    // Helper variables for Components
+    let enabled = false;
 </script>
 
-<!-- <Dialog/> -->
 <div class="container-ui-comparison">
     <div class="sub-container-button">
         <button
@@ -24,13 +27,71 @@
             >Click me</button
         >
     </div>
+    <div class="sub-container-button">
+        <Switch
+            checked={enabled}
+            on:change={(e) => (enabled = e.detail)}
+            class={enabled ? "switch switch-enabled" : "switch switch-disabled"}
+        >
+            <span class="sr-only">Enable notifications</span>
+            <span
+                class="toggle"
+                class:toggle-on={enabled}
+                class:toggle-off={!enabled}
+            />
+        </Switch>
+    </div>
+
+    <div class="sub-container-button">
+        <Dialog />
+    </div>
 </div>
 
-<!-- <h1 class="text-3xl font-bold underline">Test Heading</h1> -->
+<style>
+    :global(.switch) {
+        position: relative;
+        display: inline-flex;
+        align-items: center;
+        border-radius: 9999px;
+        height: 1.5rem;
+        width: 2.75rem;
+    }
 
-<!-- styles must be added to component which is using tailwind classes -->
-<!-- <style global lang="postcss">
-    @tailwind base;
-    @tailwind components;
-    @tailwind utilities;
-</style>  -->
+    :global(.switch-enabled) {
+        /* Blue */
+        background-color: rgb(37 99 235);
+    }
+
+    :global(.switch-disabled) {
+        /* Gray */
+        background-color: rgb(229 231 235);
+    }
+
+    .sr-only {
+        position: absolute;
+        width: 1px;
+        height: 1px;
+        padding: 0;
+        margin: -1px;
+        overflow: hidden;
+        clip: rect(0, 0, 0, 0);
+        white-space: nowrap;
+        border-width: 0;
+    }
+
+    .toggle {
+        display: inline-block;
+        width: 1rem;
+        height: 1rem;
+        background-color: rgb(255 255 255);
+        border-radius: 9999px;
+    }
+
+    .toggle-on {
+        transform: translateX(1.1rem);
+    }
+
+    .toggle-off {
+        transform: translateX(-0.25rem);
+    }
+</style>
